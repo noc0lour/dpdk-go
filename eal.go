83dfaca3c99b337cdcef7c3d2c574bab8870fd21
m.malkin <mmalkin@fun-box.ru>
2017-10-20 13:48:45 +0300

    unnecessary linkage to pcap removed

--- eal.go
+++ eal.go
@@ -2,7 +2,7 @@ package dpdk
 
 /*
 #cgo CFLAGS: -m64 -pthread -O3 -march=native -I/usr/local/include/dpdk
-#cgo LDFLAGS: -L/usr/local/lib -ldpdk -lz -lrt -lm -ldl -lfuse -lpcap
+#cgo LDFLAGS: -L/usr/local/lib -ldpdk -lz -lrt -lm -ldl -lfuse
 
 extern void go_usage_hook(char *prg);
 
